c3610ce938136c0a20424812a76b91f012a7756c
Bifdus <denimhodgson@gmail.com>
2025-02-08 22:21:07 +0800

    updated gitsigns

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,15 @@
 return {
   "folke/snacks.nvim",
-
-  --stylelua: ignore
+  opts = {
+    indent = {
+      enabled = true,
+      only_scope = false,
+    },
+    scope = { enabled = false },
+  },
+  --stylua: ignore
   keys = {
     { "<leader>sf", function() Snacks.picker.files() end, desc = "Find Files" },
   }
+,
 }
