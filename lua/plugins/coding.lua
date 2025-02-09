bd4bfd32ec1e792c9816014734e85c352ea1d055
Bifdus <denimhodgson@gmail.com>
2025-02-09 20:36:38 +0800

    feat: Barbecue

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -42,29 +42,29 @@ return {
 
   -----------------------------------------------------------------------------
   -- VS Code like winbar
-  -- {
-  --   "utilyre/barbecue.nvim",
-  --   dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
-  --   keys = {
-  --     {
-  --       "<Leader>tB",
-  --       function()
-  --         local off = vim.b["barbecue_entries"] == nil
-  --         require("barbecue.ui").toggle(off and true or nil)
-  --       end,
-  --       desc = "Breadcrumbs toggle",
-  --     },
-  --   },
-  --   opts = {
-  --     enable = true,
-  --     attach_navic = true,
-  --     show_dirname = false,
-  --     show_modified = true,
-  --     -- kinds = kind_icons, -- Uncomment if you have kind_icons defined
-  --     symbols = { separator = "" },
-  --   },
-  --   config = function(_, opts)
-  --     require("barbecue").setup(opts)
-  --   end,
-  -- },
+  {
+    "utilyre/barbecue.nvim",
+    dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "<Leader>tB",
+        function()
+          local off = vim.b["barbecue_entries"] == nil
+          require("barbecue.ui").toggle(off and true or nil)
+        end,
+        desc = "Breadcrumbs toggle",
+      },
+    },
+    opts = {
+      enable = true,
+      attach_navic = true,
+      show_dirname = false,
+      show_modified = true,
+      -- kinds = kind_icons, -- Uncomment if you have kind_icons defined
+      symbols = { separator = "" },
+    },
+    config = function(_, opts)
+      require("barbecue").setup(opts)
+    end,
+  },
 }
