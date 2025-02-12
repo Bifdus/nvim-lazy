2aacb4b51875333adf8c0c7bc379e3a3d8cbd3a4
Bifdus <denimhodgson@gmail.com>
2025-02-12 21:10:36 +0800

    updated blink mappings to remove defaults

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,11 +2,13 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      -- ["<C-k>"] = {
-      --   function(cmp)
-      --     cmp.show()
-      --   end,
-      -- },
+      preset = "none",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-y>"] = { "select_and_accept" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
     },
     completion = {
       trigger = {
