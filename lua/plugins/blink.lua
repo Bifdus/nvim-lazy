ba0536e8ef903dd564e359257ece6769604d0892
Bifdus <denimhodgson@gmail.com>
2025-02-12 23:43:21 +0800

    fix: testing blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,21 +1,26 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    -- enabled = function()
+    --   return not vim.tbl_contains({ "org", "markdown" }, vim.bo.filetype)
+    --     and vim.bo.buftype ~= "prompt"
+    --     and vim.b.completion ~= false
+    -- end,
     keymap = {
       preset = "none",
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-y>"] = { "select_and_accept" },
       ["<C-p>"] = { "select_prev", "fallback" },
       ["<C-n>"] = { "select_next", "fallback" },
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
-    completion = {
-      trigger = {
-        show_on_trigger_character = true,
-        show_on_insert_on_trigger_character = true,
-        show_on_x_blocked_trigger_characters = { "'", '"', "(", "{", "<" },
-      },
-    },
+    -- completion = {
+    --   trigger = {
+    --     show_on_trigger_character = true,
+    --     show_on_insert_on_trigger_character = true,
+    --     show_on_x_blocked_trigger_characters = { "'", '"', "(", "{", "<" },
+    --   },
+    -- },
   },
 }
