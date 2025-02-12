de228da709e3c9d2210e1fd1566966a56d946768
Bifdus <denimhodgson@gmail.com>
2025-02-12 23:16:02 +0800

    fix: Lsp configuration for php and pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "snacks"
+vim.g.lazyvim_php_lsp = "intelephense"
+vim.g.lazyvim_python_lsp = "basedpyright"
